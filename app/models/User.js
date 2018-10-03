7bb8df3570223c55b4dc9ab01cb4d2eed3ab338e
mamteshku <37476408+mamteshku@users.noreply.github.com>
2018-10-03 16:33:43 +0530

    All Files have been uploaded

---
+++ app/models/User.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module Dependencies Created By Mamtesh kumar 28-9-2018
+ */
+const mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  userId: {
+    type: String,
+    default: '',
+    index: true,
+    unique: true
+  },
+  firstName: {
+    type: String,
+    default: ''
+  },
+  lastName: {
+    type: String,
+    default: ''
+  },
+  password: {
+    type: String,
+    default: 'passskdajakdjkadsj'
+  },
+  email: {
+    type: String,
+    default: ''
+  },
+  mobileNumber: {
+    type: Number,
+    default: 0
+  },
+  createdOn :{
+    type:Date,
+    default:""
+  }
+
+
+})
+
+
+mongoose.model('User', userSchema);
